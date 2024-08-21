93c9d2325135dfd23de03b7ccf7bf8002d49cc12
Rai Nogueira <rai98.nogueira@gmail.com>
2024-08-21 16:31:53 -0300

    inclusão do projeto

---
+++ src/test/java/elementos/ElementosWeb.java
@@ -0,0 +1,13 @@
+package elementos;
+
+import org.openqa.selenium.By;
+
+public class ElementosWeb {
+	
+	By btnLogin = By.xpath("//button[@class='login-button']");
+    By username = By.xpath("//input[@name='username']");
+    
+    // eu vou vencer!!
+	
+	
+}
