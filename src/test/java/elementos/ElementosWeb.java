d54c20fab23e6b7e32c6e5cdbdd818b2f7db33b3
Rai Nogueira <rai98.nogueira@gmail.com>
2024-08-22 21:51:01 -0300

    inclusão de elementos login

--- src/test/java/elementos/ElementosWeb.java
+++ src/test/java/elementos/ElementosWeb.java
@@ -4,11 +4,17 @@ import org.openqa.selenium.By;
 
 public class ElementosWeb {
 	
+	// Elementos Login
 	By btnLogin = By.xpath("//button[@class='login-button']");
     By username = By.xpath("//input[@name='username']");
-    
-    // eu vou vencer!!
-    // sim eu vou vencer!!
+    By password = By.xpath("//input[@name='password']");
+    By btnContinuar = By.xpath("//button[@class='form-button']");
+    By btnCancelar = By.xpath("//button[contains(text(),'Cancelar')]");
+
+    // Elementos Abrir conta
+	By btnAbrirConta = By.xpath("//button[@class='login-button']");
+	By cpf = By.xpath("//input[@id='cpf-input']");
+
 	
 	
 }
